4446191bbfdcc3e3203f224f2ee723b33151f86d
BastianVino <143992655+AlvinoValerian@users.noreply.github.com>
2026-04-18 11:37:23 +0700

    js-15 done (#1)

--- src/utlis/db/servicefirebase.ts
+++ src/utlis/db/servicefirebase.ts
@@ -1,19 +1,77 @@
-import { getFirestore, collection, getDocs , Firestore, getDoc, doc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Firestore,
+  getDoc,
+  doc,
+  query,
+  addDoc,
+  where,
+} from "firebase/firestore";
 import app from "./firebase";
+import bcrypt from "bcrypt";
+import { MessageChannel } from "worker_threads";
+import { divide } from "firebase/firestore/pipelines";
+import { Elsie } from "next/font/google";
 
 const db = getFirestore(app);
 
-export async function retriveProducts(collectionName: string) {
+export async function retrieveProducts(collectionName: string) {
   const snapshot = await getDocs(collection(db, collectionName));
-  const data = snapshot.docs.map((doc) => ({ 
+  const data = snapshot.docs.map((doc) => ({
     id: doc.id,
-     ...doc.data(),
-     }));
-     return data;
+    ...doc.data(),
+  }));
+  return data;
 }
 
 export async function retrieveDataByID(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(db, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    password: string;
+    role?: string;
+  },
+  callback: Function,
+) {
+  const q = query(
+    collection(db, "users"),
+    where("email", "==", userData.email)
+  );
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (data.length > 0) {
+  // user sudah ada
+  callback({
+    status: "error",
+    message: "User already exists",
+  });
+} else {
+  userData.password = await bcrypt.hash(userData.password,10);
+  userData.role = "member";
+  await addDoc(collection(db,"users"),userData)
+  .then(() => {
+    callback({
+      status: "success",
+      message: "User registered successfully",
+    });
+  })
+  .catch((error) => {
+    callback({
+      status: "error",
+      message: error.message,
+    });
+  });
+}
 }
--- src/views/auth/register/index.tsx
+++ src/views/auth/register/index.tsx
@@ -1,13 +1,64 @@
 import Link from "next/link";
 import styles from "../../auth/register/register.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const TampilanRegister = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    setError("");
+    setIsLoading(true);
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const fullname = formData.get("Fullname") as string;
+    const password = formData.get("Password") as string;
+
+    if (!email) {
+    setError("Email wajib diisi");
+    setIsLoading(false);
+    return;
+  }
+
+  if (!password || password.length < 6) {
+    setError("Password minimal 6 karakter");
+    setIsLoading(false);
+    return;
+  }
+
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, fullname, password }),
+    });
+
+    if (response.status === 200) {
+      form.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError(
+        response.status === 400
+          ? "Email already exists"
+          : "An error occurred"
+      );
+    }
+  };
+
   return (
     <div className={styles.register}>
+      {error && <p className={styles.register__error}>{error}</p>}
       <h1 className={styles.register__title}>Halaman Register</h1>
       <div className={styles.register__form}>
-
-      <form action="">
+        <form onSubmit={handleSubmit}>
         
       <div className={styles.register__form__item}>
         <label htmlFor="email" className={styles.register__form__item__label}>
@@ -45,8 +96,12 @@ const TampilanRegister = () => {
         className={styles.register__form__item__input} />
       </div>
 
-      <button type="submit" className={styles.register__form__item__button}>
-        Register
+      <button 
+      type="submit" 
+      className={styles.register__form__item__button}
+      disabled={isLoading}
+      >
+        {isLoading ? "Loading..." : "Register"}
       </button>
 
       </form>
